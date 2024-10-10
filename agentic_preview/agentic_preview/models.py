4dda59806d1e7e7d1aaa67f4c44e1e905a6b8a94
rUv <ruv@ruv.net>
2024-10-10 19:58:32 +0000

    feat: Update Project model to use UUID for id field
    The Project model in the agentic_preview application has been updated to use UUID for the id field instead of a regular string. This change ensures uniqueness and improves the security of the identifiers. The id field now has a default value generated using the uuid4 function from the uuid module.
    
    # Please enter the commit message for your changes. Lines starting
    # with '#' will be ignored, and an empty message aborts the commit.
    # On branch main
    # Your branch is up to date with 'origin/main'.
    #
    # Changes to be committed:
    #       modified:   agentic_preview/agentic_preview/models.py
    #

--- agentic_preview/agentic_preview/models.py
+++ agentic_preview/agentic_preview/models.py
@@ -1,13 +1,14 @@
 from sqlalchemy import Column, String, DateTime, Float
 from sqlalchemy.ext.declarative import declarative_base
 from datetime import datetime
+import uuid
 
 Base = declarative_base()
 
 class Project(Base):
     __tablename__ = "projects"
 
-    id = Column(String, primary_key=True, index=True)
+    id = Column(String(36), primary_key=True, default=lambda: str(uuid.uuid4()), index=True)
     name = Column(String, index=True)
     user_id = Column(String, index=True)
     repo_url = Column(String)
